9542f960aa3910c37b67424f3fed17c6f1acf39c
leovoon <voonlihhaur@gmail.com>
2020-09-01 00:43:15 +0800

    Updates

---
+++ precache-manifest.1b266a2f1b4925f3b430592e4d907d04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af0ea077645fcc1ca4997dbcec33da9",
+    "url": "/holiday-react-app/index.html"
+  },
+  {
+    "revision": "87d5c046847fa6464b6d",
+    "url": "/holiday-react-app/static/css/main.b8ba89e2.chunk.css"
+  },
+  {
+    "revision": "dc28821af32170983a59",
+    "url": "/holiday-react-app/static/js/2.86889368.chunk.js"
+  },
+  {
+    "revision": "9a8a64d30a81695e7691dd446a703cba",
+    "url": "/holiday-react-app/static/js/2.86889368.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87d5c046847fa6464b6d",
+    "url": "/holiday-react-app/static/js/main.15843b6e.chunk.js"
+  },
+  {
+    "revision": "ffed715f8620daebeaef",
+    "url": "/holiday-react-app/static/js/runtime-main.29270642.js"
+  },
+  {
+    "revision": "b2e431c36a4ec642079d9876a51f0938",
+    "url": "/holiday-react-app/static/media/logo.b2e431c3.svg"
+  }
+]);
