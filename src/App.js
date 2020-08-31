af0aaf05edba071289ac4607b314dc22f61eafd2
leovoon <voonlihhaur@gmail.com>
2020-09-01 00:39:18 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import Moment from "react-moment";
+import { useFetch } from "./useFetchHook";
+require('dotenv').config()
+
 
 function App() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const url = `
+  https://cors-anywhere.herokuapp.com/calendarific.com/api/v2/holidays?&api_key=${process.env.REACT_APP_NOT_SECRET_CODE}&country=MY&year=${year}&month=${month}&day=${day}`;
+
+  
+  const { holidays, loading } = useFetch(url);
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+    <div className='App'>
+    <h1 style={{ textTransform: 'upperCase' }}>
+What Holiday is Today in <span role="img" aria-labelledby="checking holiday" >🇲🇾</span></h1>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Moment date={date} format='DD  MMMM YYYY' />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+        {!loading ? holidays === undefined 
+          ? "Today isn't a holiday." : <div>{holidays[0].name}</div> :
+          <div>Checking..<span role="img" aria-labelledby="checking holiday" >💭</span></div>
+          }
       </header>
     </div>
   );
